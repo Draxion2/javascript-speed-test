e7106fc7fa46c21a86557e93f96c0ac5567c88cd
= <holdanmcmullen@gmail.com>
2022-02-16 03:24:00 -0700

    added all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var quizAnswers = document.querySelector(".quiz-answers");
 var quizSummary = document.querySelector(".quiz-summary");
 var quizTitle = document.querySelector(".quiz-title");
 var result = document.querySelector(".results");
+var allQuestions = document.querySelectorAll(".questions");
 var startBtn = document.getElementById("start-btn");
 var timerDisplay = document.getElementById("timer");
 var questionA = document.getElementById("questionA");
@@ -14,7 +15,7 @@ var questionD = document.getElementById("questionD");
 // config numerials
 var questionIndex = 0;
 var isCorrect = 0;
-var timer = 75;
+var timer = 5;
 var pen = 0;
 
 // config questions
@@ -33,8 +34,82 @@ var questions = [{
         { text: "// text", isCorrect: true },
         { text: "...", isCorrect: false }
     ]
+},
+{
+    q: "Which of the following operators is used to divide integers?",
+    a: [{ text: "*", isCorrect: false },
+        { text: "/", isCorrect: true },
+        { text: "=", isCorrect: false },
+        { text: "%", isCorrect: false }
+    ]
+},
+{
+    q: "What comparison operator is used to determine if the type AND value are equal to true?",
+    a: [{ text: "=", isCorrect: false },
+        { text: "=+", isCorrect: false },
+        { text: "==", isCorrect: false },
+        { text: "===", isCorrect: true }
+    ]
+},
+{
+    q: "What is the NaN property?",
+    a: [{ text: "A statement that returns a number", isCorrect: false },
+        { text: "A function", isCorrect: false },
+        { text: "A value that represents Not-a-Number", isCorrect: true },
+        { text: "A negative integer", isCorrect: false }
+    ]
+},
+{
+    q: "What does the DOM stand for?",
+    a: [{ text: "Document Object Model", isCorrect: true },
+        { text: "Determining Operator Moments", isCorrect: false },
+        { text: "Document Order Module", isCorrect: false },
+        { text: "Domain Object Menu", isCorrect: false }
+    ]
+},
+{
+    q: "What does a global scope variable mean?",
+    a: [{ text: "A variable that can only be accessed by an admin", isCorrect: false },
+        { text: "A variable that can be called anywhere on the script", isCorrect: true },
+        { text: "A variable that can only be accessed within a function", isCorrect: false },
+        { text: "A variable that returns undefined", isCorrect: false }
+    ]
+},
+{
+    q: "Which method is used to store data to the browser?",
+    a: [{ text: "console.log()", isCorrect: false },
+        { text: "alert()", isCorrect: false },
+        { text: "JSON()", isCorrect: false },
+        { text: "localStorage()", isCorrect: true }
+    ]
+},
+{
+    q: "Which of the following IS NOT a data type?",
+    a: [{ text: "window", isCorrect: true },
+        { text: "Null", isCorrect: false },
+        { text: "Boolean", isCorrect: false },
+        { text: "String", isCorrect: false }
+    ]
+},
+{
+    q: "Explain what are escape characters.",
+    a: [{ text: "They are characters used in a password", isCorrect: false },
+        { text: "They are characters used with a backslash so they can be displayed", isCorrect: true },
+        { text: "Characters in strings used to split into two separate strings", isCorrect: false },
+        { text: "A string that is strict to only using letters", isCorrect: false }
+    ]
+}];
+
+var gameOver = function() {
+    // show summary and start button
+    quizAnswers.style.display = "none";
+    quizSummary.style.display = "block";
+    startBtn.style.display = "block";
+    quizTitle.textContent = "Quiz Challenge";
+    timer = 0;
+    timerDisplay.textContent = timer;
+    alert("GAME OVER");
 }
-];
 
 // run questions
 var runQuestions = function() {
@@ -66,8 +141,7 @@ var startQuestions = function() {
         // if count is at 0
         if (timer <= 0) {
             clearInterval(countDown);
-            timerDisplay.textContent = timer;
-            alert("GAME OVER");
+            gameOver();
             return;
         }
 
@@ -75,10 +149,8 @@ var startQuestions = function() {
         if (pen !== 0) {
             timer = timer - pen;
             if (timer <= 0) {
-                alert("GAME OVER");
                 clearInterval(countDown);
-                timer = 0;
-                timerDisplay.textContent = timer;
+                gameOver();
                 return;
             }
             timerDisplay.textContent = timer;
@@ -102,6 +174,8 @@ quizAnswers.addEventListener("click", function(event) {
     if (questionClicked === "false") {
         result.textContent = "WRONG!";
         pen = 10;
+        questionIndex++;
+        runQuestions();
 
     // answer is correct
     } else {
